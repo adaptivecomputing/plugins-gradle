15c26c8826311248b626d6f01431ccafaa2aeb94
Brian Saville <bsaville@adaptivecomputing.com>
2013-04-23 07:52:38 -0600

    Fix compilation error

--- nexus-workflow/src/main/groovy/com/adaptc/gradle/nexusworkflow/NexusWorkflowExtension.groovy
+++ nexus-workflow/src/main/groovy/com/adaptc/gradle/nexusworkflow/NexusWorkflowExtension.groovy
@@ -1,11 +1,5 @@
 package com.adaptc.gradle.nexusworkflow
 
-import org.gradle.api.Project
-import org.gradle.api.plugins.JavaPluginConvention
-import org.gradle.api.reporting.ReportingExtension
-import org.gradle.api.tasks.SourceSet
-import org.gradle.api.plugins.cobertura.CoberturaPluginExtension
-
 /**
  * @author bsaville
  */
